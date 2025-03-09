7a43a2185fa53da2843b8afc7316d41d3807da23
anajmrocki <anajmrocki@MacBook-Pro-anajmrocki.local>
2025-03-09 21:59:39 +0100

    Setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl text-primary-500 font-black text-light400_light500">
-    Welcome to the world of Next.js
-  </h1>
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the world of Next.js SPACE GROTESK
+    </h1>
+  </>
 );
 
 export default Home;
