34920e4baf1a3ead3102217156737d85598dfac4
anajmrocki <anajmrocki@MacBook-Pro-anajmrocki.local>
2025-03-09 21:34:48 +0100

    TailwindCSS styling and theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl text-white font-black">
+  <h1 className="text-3xl text-primary-500 font-black text-light400_light500">
     Welcome to the world of Next.js
   </h1>
 );
