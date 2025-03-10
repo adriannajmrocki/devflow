334baf4679e8a074cf99df5b8cc416ac3788bf1a
anajmrocki <anajmrocki@MacBook-Pro-anajmrocki.local>
2025-03-10 22:27:46 +0100

    Assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,19 @@ import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
-  // subsets: ["latin"],
 });
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
-  // subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of Stack Overflow.",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
