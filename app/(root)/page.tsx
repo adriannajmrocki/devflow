8a61f35cc060bd68b3b70e7d3f9ed096eb9d0744
anajmrocki <anajmrocki@192.168.1.7>
2025-03-30 13:04:30 +0200

    Mobile navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -12,16 +10,6 @@ const Home = async () => {
       <h1 className="h1-bold font-space-grotesk">
         Welcome to the world of Next.js SPACE GROTESK
       </h1>
-      <form
-        className="px-10 py-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
