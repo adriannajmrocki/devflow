9a606eca54a6ba70f4b98779883a6cb8a66a6377
anajmrocki <anajmrocki@MacBook-Pro-anajmrocki.local>
2025-04-27 20:31:09 +0200

    Implement LeftSidebar and RightSidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -8,6 +8,8 @@ const ROUTES = {
   TAGS: "/tags",
   PROFILE: "/profile",
   ASK_QUESTION: "/ask-question",
+  QUESTION: (id: string) => `/question/${id}`,
+  TAG: (id: string) => `/tags/${id}`,
 };
 
 export default ROUTES;
